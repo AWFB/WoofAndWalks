29a9ad31c2f51c66dc4ea259c9a1aa632fed2f3a
Antony Beardsmore <sider@me.com>
2022-10-13 17:14:07 +0100

    API created

---
+++ WoofsAndWalksAPI/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WoofsAndWalksAPI.Data;
+using WoofsAndWalksAPI.Models;
+
+namespace WoofsAndWalksAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetSingleUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+}
---
+++ WoofsAndWalksAPI/RegisterServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WoofsAndWalksAPI.Data;
+
+namespace WoofsAndWalksAPI;
+
+public static class RegisterServices
+{
+    public static void ConfigureInjection(this WebApplicationBuilder builder)
+    {
+        /* Database Context Dependency Injection */
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+       
+        // auth
+
+    }
+}
